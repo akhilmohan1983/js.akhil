66a6a091a69000313b39e93ab94426d295c78f51
akhilmohan1983 <109690285+akhilmohan1983@users.noreply.github.com>
2024-02-24 18:06:03 +0000

    num change

---
+++ 01_basics/stringtest.js
@@ -0,0 +1,9 @@
+const name = "Akhil"
+const repocount=50
+//console.log(name+repocount)
+const gameName = new String('akhil')
+//console.log(gameName)
+//console.log(gameName[0])
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(`My name is ${name} and my age is ${repocount} `)
