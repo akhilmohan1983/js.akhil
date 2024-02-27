8efbe2dd5f8d9d0c53720ae19d850c7411130fc3
akhilmohan1983 <109690285+akhilmohan1983@users.noreply.github.com>
2024-02-27 09:08:22 +0000

    arrayexample

---
+++ 01_basics/arraytest.js
@@ -0,0 +1,13 @@
+let fruits = ["apple","mango","pinaple","banana"]
+for(let i =0;i<=fruits.length-1; i++){
+//console.log(fruits[i])
+}
+let fruits2=[]
+for(let i =0;i<=fruits.length-1; i++){
+    fruits2.push(fruits[i])
+}
+//console.log(fruits2)
+console.log(fruits)
+let [fruits3,fruits4,...fruits5]=fruits
+console.log(fruits3)
+console.log(fruits5)
