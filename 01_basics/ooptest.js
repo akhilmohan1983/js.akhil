22e2e8de4cb2b68bacc774a9908a5d5ea820afc8
akhilmohan1983 <109690285+akhilmohan1983@users.noreply.github.com>
2024-03-14 07:21:40 +0000

    today14

---
+++ 01_basics/ooptest.js
@@ -0,0 +1,23 @@
+//call apply bind
+// function hello(){
+//     console.log("Hello world")
+// }
+// hello.call();
+function about(hobby,favmusic){
+    console.log(this.Name, this.Age, hobby, favmusic)
+}
+const user1 = {
+    Name: "Akhil",
+    Age:8,
+    
+}
+
+const user2 = {
+    Name: "Kay",
+    Age:15,
+
+    }
+
+//about.apply(user2,["bond","kong"])
+const funcme = about.bind(user2,"bong","kong")
+funcme();
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,26 @@
-console.log("akhil")
+const personinfo = function(){
+    console.log(`Person name is ${this.myname} and age is ${this.age}`)
+}
+const person1 ={
+    myname: "Harsh",
+    age:10,
+    Gender:"male",
+    about:personinfo   
+}
+const person2 ={
+    myname: "Akhil",
+    age:16,
+    Gender:"male",
+    about:personinfo   
+}
+const person3 ={
+    myname: "Sahil",
+    age:11,
+    Gender:"male",
+    about:personinfo   
+}
+
+
+person1.about();
+person2.about();
+//console.log(this.about)
